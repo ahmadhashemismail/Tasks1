c3d6be189e0dc1c37f480637792f5323486ac7f7
Ahmad ismail <ahmadhashemismail1@gmail.com>
2026-08-18 23:38:05 +0300

    update to use env v2d;;;;

---
+++ server/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE accounts (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    note TEXT,
+    date DATE,
+    enddate DATE,
+    done BOOLEAN DEFAULT FALSE
+);
