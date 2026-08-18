5bec3570545c0aa2a0faf645e7f8cdaa75e60efd
Ahmad ismail <ahmadhashemismail1@gmail.com>
2026-08-18 23:26:21 +0300

    update to use env v2d;;

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,5 @@
-const BASE = "http://localhost:4000/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+const BASE = `${API_URL}/api`;
 
 export async function getTasks() {
     const res = await fetch(BASE + "/tasks" ,{
