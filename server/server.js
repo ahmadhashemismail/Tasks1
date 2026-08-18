bfa5c0a47a2a8afba401f2580e1c9ad5f09c79bf
Ahmad ismail <ahmadhashemismail1@gmail.com>
2026-08-18 23:16:19 +0300

    update to use env v2

--- server/server.js
+++ server/server.js
@@ -129,6 +129,7 @@ app.post("/api/accounts/login", async (req, res) => {
   }
 });
 
-app.listen(4000, () => {
-  console.log("Server running on port 4000");
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
