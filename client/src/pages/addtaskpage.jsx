e13df187f1a951bbe46f4563db15005262d593a3
Ahmad ismail <ahmadhashemismail1@gmail.com>
2026-08-20 03:46:46 +0300

    update in addtasklist

--- client/src/pages/addtaskpage.jsx
+++ client/src/pages/addtaskpage.jsx
@@ -22,6 +22,7 @@ function AddTaskPage() {
   function handletime(date){
     let startdate=  new Date(date);
   let datenow=new Date();
+   datenow.setHours(0, 0, 0, 0);
     setError(!(startdate >= datenow));
     
   }
